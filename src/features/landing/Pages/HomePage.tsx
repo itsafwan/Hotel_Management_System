eb8bca46f87f85451085bace31b8406d36b729b8
Msafwan001 <muhammadsafwan20051@gmail.com>
2026-02-25 18:04:38 +0500

    second commit

---
+++ src/features/landing/Pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import ResponsiveAppBar from "../components/Navbar";
+function HomePage() {
+  return (
+    <>
+  <ResponsiveAppBar/>
+      <div>
+        <h1>Hotel Management System</h1>
+        <p>Welcome to our hotel website</p>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
